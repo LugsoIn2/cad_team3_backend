65f12086771e687eaf6bb295b87926c31b8690dd
Timmy <timmy.pfau@netpy.de>
2022-10-10 18:04:53 +0200

    add djangocors

--- cad_team3_backend/cad_team3_backend/settings.py
+++ cad_team3_backend/cad_team3_backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'file_app'
+    'file_app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'cad_team3_backend.urls'
@@ -135,3 +138,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
